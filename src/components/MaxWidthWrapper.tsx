8b83f17fe2c3335c846b6cb98c776d4a0c778b85
nothing-available <smtkur31@gmail.com>
2024-07-06 21:26:05 +0530

    Add utility function for merging CSS classes to improve code readability and maintainability

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,16 @@
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+const MaxWidthWrapper = ({
+    classname,
+    children,
+}:{
+    classname?: string,
+    children: ReactNode,
+}) =>{
+    <div className={cn("mx-auto w-full max-w-screen-xl px-2.5 md:px-20",classname)}>
+        {children}
+    </div>
+}
+
+export default MaxWidthWrapper; 
