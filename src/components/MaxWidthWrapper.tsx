f0285d628fc6cc9f3a5e31898cba679399a0d2d1
nothing-available <smtkur31@gmail.com>
2024-07-09 21:48:27 +0530

    Landing page

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -1,16 +1,17 @@
-import { cn } from "@/lib/utils";
-import { ReactNode } from "react";
+import { ReactNode } from 'react'
+import { cn } from '@/lib/utils'
 
 const MaxWidthWrapper = ({
-    classname,
+    className,
     children,
-}:{
-    classname?: string,
-    children: ReactNode,
-}) =>{
-    <div className={cn("mx-auto w-full max-w-screen-xl px-2.5 md:px-20",classname)}>
+}: {
+    className? : string
+    children: ReactNode
+}) => {
+    return(
+    <div className={cn('mx-auto w-full max-w-screen-xl px-2.5 md:px-20', className)}>
         {children}
     </div>
-}
+)}
 
-export default MaxWidthWrapper; 
+export default MaxWidthWrapper;
